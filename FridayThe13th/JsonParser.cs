d0989bcb05d2de0772e41767ee7b1e4fa5b87233
JP Richardson <jprichardson@gmail.com>
2011-08-04 15:45:51 -0500

    Initial commit with test project.

---
+++ FridayThe13th/JsonParser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FridayThe13th {
+	public class JsonParser {
+
+		public JsonParser(string jsonText) {
+
+		}
+
+		public dynamic Parse() {
+			return null;
+		}
+
+	}
+}
---
+++ TestFridayThe13th/JsonParserTest.cs
@@ -0,0 +1,37 @@
+﻿using FridayThe13th;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestFridayThe13th
+{
+
+	[TestClass()]
+	public class JsonParserTest {
+
+		private TestContext testContextInstance;
+
+		public TestContext TestContext {
+			get {
+				return testContextInstance;
+			}
+			set {
+				testContextInstance = value;
+			}
+		}
+
+		[TestInitialize()]
+		public void MyTestInitialize()
+		{
+		}
+
+		[TestCleanup()]
+		public void MyTestCleanup()
+		{
+		}
+
+		[TestMethod()]
+		public void ParseTest() {
+			
+		}
+	}
+}
