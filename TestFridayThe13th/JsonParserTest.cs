9d0397781ee4dda7c09f3c088bb7cda5dcbd46f6
JP Richardson <jprichardson@gmail.com>
2011-08-04 15:53:04 -0500

    Setup first test.

--- TestFridayThe13th/JsonParserTest.cs
+++ TestFridayThe13th/JsonParserTest.cs
@@ -30,8 +30,19 @@ namespace TestFridayThe13th
 		}
 
 		[TestMethod()]
-		public void ParseTest() {
-			
+		public void Parse2Test() {
+			var jsp = new JsonParser(Properties.Resources.example2);
+
+			dynamic res = jsp.Parse();
+
+			Assert.AreEqual("file", res.menu.id);
+			Assert.AreEqual("File", res.menu.value);
+			Assert.AreEqual("New", res.menu.popup.menuitems[0].value);
+			Assert.AreEqual("Open", res.menu.popup.menuitems[1].value);
+			Assert.AreEqual("Close", res.menu.popup.menuitems[2].value);
+			Assert.AreEqual("CreateNewDoc()", res.menu.popup.menuitems[0].onclick);
+			Assert.AreEqual("OpenDoc()", res.menu.popup.menuitems[1].onclick);
+			Assert.AreEqual("CloseDoc()", res.menu.popup.menuitems[2].onclick);
 		}
 	}
 }
